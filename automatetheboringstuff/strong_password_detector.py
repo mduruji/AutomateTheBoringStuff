d05f7b4d7d8a0faf1cf115a60065c94d64beed5e
mduruji <mczduruji@gmail.com>
2024-11-28 07:30:08 -0500

    made strong password detection regex

--- automatetheboringstuff/strong_password_detector.py
+++ automatetheboringstuff/strong_password_detector.py
@@ -5,3 +5,23 @@ A strong password is defined as one that is at least eight characters long,
 contains both uppercase and lowercase characters, and has at least one digit.
 You may need to test the string against multiple regex patterns to validate its strength.
 """
+
+import re
+
+
+def is_strong_password(password):
+    length_regex = re.compile(r'.{8,}')
+
+    lowercase_regex = re.compile(r'[a-z]')
+
+    uppercase_regex = re.compile(r'[A-Z]')
+
+    digit_regex = re.compile(r'\d')
+
+    if (length_regex.search(password) and
+        lowercase_regex.search(password) and
+        uppercase_regex.search(password) and
+        digit_regex.search(password)):
+        return True
+    else:
+        return False
